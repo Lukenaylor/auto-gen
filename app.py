c89607956812818f323e7f54736af4fb2836b2bd
Lukenaylor <110476174+Lukenaylor@users.noreply.github.com>
2023-11-08 18:06:38 +0000

    group chat

--- app.py
+++ app.py
@@ -4,53 +4,60 @@ import autogen
 config_list = [
     {
     'model':'gpt-3.5-turbo-16k',
-    'api_key':'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'
+    'api_key':'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'
     }
 ]
 
-#2 create an AssistantAgent names "assistant"
-assistant = autogen.AssistantAgent(
-    name="assistant",
-    llm_config={
-        "seed": 41, # seed for caching and reproducibility
-        "config_list": config_list,
-        "temperature":0, # temperature of sampling, this ideally get changed via an API in the future
-    },
-)
-
-proof_reader = autogen.AssistantAgent(
-    name="proof reader",
-    llm_config={
-        "seed": 44,
-        "config_list": config_list,
-        "temperature":0,
-    },
-)
+llm_config = {
+    "seed": 42,  # change the seed for different trials
+    "temperature": 0,
+    "config_list": config_list,
+    "timeout": 120,
+}
 
 user_proxy = autogen.UserProxyAgent(
-    name="user_proxy",
-    human_input_mode='NEVER',
-    max_consecutive_auto_reply=5,
-    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
-    code_execution_config={
-        "work_dir":"coding",
-        "use_docker": False,
-    },
+   name="Admin",
+   system_message="A human admin. Interact with the planner to discuss the plan. Plan execution needs to be approved by this admin.",
+   code_execution_config=False,
 )
+engineer = autogen.AssistantAgent(
+    name="Engineer",
+    llm_config=llm_config,
+    system_message='''Engineer. You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor.
+Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor.
+If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
+''',
+)
+scientist = autogen.AssistantAgent(
+    name="Scientist",
+    llm_config=llm_config,
+    system_message="""Scientist. You follow an approved plan. You are able to categorize papers after seeing their abstracts printed. You don't write code."""
+)
+planner = autogen.AssistantAgent(
+    name="Planner",
+    system_message='''Planner. Suggest a plan. Revise the plan based on feedback from admin and critic, until admin approval.
+The plan may involve an engineer who can write code and a scientist who doesn't write code.
+Explain the plan first. Be clear which step is performed by an engineer, and which step is performed by a scientist.
+''',
+    llm_config=llm_config,
+)
+executor = autogen.UserProxyAgent(
+    name="Executor",
+    system_message="Executor. Execute the code written by the engineer and report the result.",
+    human_input_mode="NEVER",
+    code_execution_config={"last_n_messages": 3, "work_dir": "paper"},
+)
+critic = autogen.AssistantAgent(
+    name="Critic",
+    system_message="Critic. Double check plan, claims, code from other agents and provide feedback. Check whether the plan includes adding verifiable info such as source URL.",
+    llm_config=llm_config,
+)
+groupchat = autogen.GroupChat(agents=[user_proxy, engineer, scientist, planner, executor, critic], messages=[], max_round=50)
+manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llm_config)
 
 user_proxy.initiate_chat(
-    assistant,
-    message="""Provide a 500 word blog in text format about accessibility and it's importance in eLearning""",
-)
-
-#follow up from the first response
-
-user_proxy.send(
-    recipient=assistant,
-    message="""Please can you write a linkedin marketing message on the blog post and suggest some SEO tags""",
-)
-
-user_proxy.send(
-    recipient=proof_reader,
-    message="please can you go through and check the responses and change any US english to UK english, and pass this back to the Assistant"
+    manager,
+    message="""
+find papers on LLM applications from arxiv in the last week, create a markdown table of different domains.
+""",
 )
