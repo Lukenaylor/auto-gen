fe29e646ec81b5fc89ff753b468551815a29faa5
Lukenaylor <110476174+Lukenaylor@users.noreply.github.com>
2023-11-09 11:14:44 +0000

    6 agent flow - 1st run

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import autogen
 config_list = [
     {
     'model':'gpt-3.5-turbo-16k',
-    'api_key':'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'
+    'api_key':'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'
     }
 ]
 
@@ -12,7 +12,7 @@ llm_config = {
     "seed": 42,  # change the seed for different trials
     "temperature": 0,
     "config_list": config_list,
-    "timeout": 120,
+    "timeout": 300,
 }
 
 user_proxy = autogen.UserProxyAgent(
@@ -20,44 +20,40 @@ user_proxy = autogen.UserProxyAgent(
    system_message="A human admin. Interact with the planner to discuss the plan. Plan execution needs to be approved by this admin.",
    code_execution_config=False,
 )
-engineer = autogen.AssistantAgent(
-    name="Engineer",
+instructional_designer = autogen.AssistantAgent(
+    name="Instructional_Designer",
     llm_config=llm_config,
-    system_message='''Engineer. You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor.
-Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor.
-If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
+    system_message='''You are an experienced eLearning instructional designer that has extensive knowledge about how to deliver engaging content to learners across a range of subjects. You take the information from the Admin. Do not ask others to copy and paste the result. Check the research result returned by the researcher.
+If the result indicates there is an error, fix the error and output the fixed response. If the error can't be fixed, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
 ''',
 )
-scientist = autogen.AssistantAgent(
-    name="Scientist",
+researcher = autogen.AssistantAgent(
+    name="Researcher",
     llm_config=llm_config,
-    system_message="""Scientist. You follow an approved plan. You are able to categorize papers after seeing their abstracts printed. You don't write code."""
+    system_message="""You are an experienced eLearning content researcher that has the ability to generate accurate research across a range of subjects that the instructional designer can use to create engaging eLearning content. You follow an approved plan. You are able to find and categorize relavent subject knowledge."""
 )
 planner = autogen.AssistantAgent(
     name="Planner",
-    system_message='''Planner. Suggest a plan. Revise the plan based on feedback from admin and critic, until admin approval.
-The plan may involve an engineer who can write code and a scientist who doesn't write code.
-Explain the plan first. Be clear which step is performed by an engineer, and which step is performed by a scientist.
-''',
+    system_message='''You are an experienced eLearning project manager, that is able to understand technical and non-technical knowledge. Your role is to use information provided by the instructional designer and create a plan that a human eLearning Storyline 360 developer can use to build a WCAG AA compliant course. Revise the plan based on feedback from admin and critic, until admin approval.''',
     llm_config=llm_config,
 )
-executor = autogen.UserProxyAgent(
-    name="Executor",
-    system_message="Executor. Execute the code written by the engineer and report the result.",
+proof_reader = autogen.UserProxyAgent(
+    name="Proof_Reader",
+    system_message="You are an experienced proof reader with a focus on UK english. Your role is to assess the output provided by the instructional designer and suggest changes to make the elearning course as simple to follow as possible. Your role is to focus on the way the eLearning course is written, not to change the overall content.",
     human_input_mode="NEVER",
     code_execution_config={"last_n_messages": 3, "work_dir": "paper"},
 )
 critic = autogen.AssistantAgent(
     name="Critic",
-    system_message="Critic. Double check plan, claims, code from other agents and provide feedback. Check whether the plan includes adding verifiable info such as source URL.",
+    system_message="Critic. Double check plan, claims, and responses from other agents and provide feedback. Check whether the plan includes adding verifiable info such as source URL. At the end of the process, please compile all the responses into one overall, useable text file that can be exported to google docs.",
     llm_config=llm_config,
 )
-groupchat = autogen.GroupChat(agents=[user_proxy, engineer, scientist, planner, executor, critic], messages=[], max_round=50)
+groupchat = autogen.GroupChat(agents=[user_proxy, instructional_designer, researcher, planner, proof_reader, critic], messages=[], max_round=50)
 manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llm_config)
 
 user_proxy.initiate_chat(
     manager,
     message="""
-find papers on LLM applications from arxiv in the last week, create a markdown table of different domains.
+Please create the storyboard for a 30 minute eLearning course about sanctions regulations. The course should be as engaging as possible for learners and should use the most up to date learning design techniques. Note this storyboard is completely text based and should not assume any prior learner knowledge about the subject. As part of this storyboard, please include a question bank of 20 relavent multi choice questions.
 """,
 )
